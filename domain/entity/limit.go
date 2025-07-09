895000bf2346194a08c4da76f54035930f388520
sayyid <sayyid.visiku@gmail.com>
2025-07-09 17:47:31 +0700

    ♻️ Refactor Limit Entity

--- domain/entity/limit.go
+++ domain/entity/limit.go
@@ -1,30 +1,27 @@
 package entity
 
 import (
-	"github.com/google/uuid"
+	"time"
+
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
 )
 
 type Limit struct {
-	ID          uint            `json:"id" gorm:"primaryKey"`
-	UUID        uuid.UUID       `json:"uuid" gorm:"uniqueIndex;type:char(36)"`
-	UserID      uint            `json:"user_id"`
-	Tenor       uint            `json:"tenor" gorm:"type:smallint unsigned"`
-	LimitAmount decimal.Decimal `json:"limit_amount" gorm:"type:decimal(20,2)"`
-	User        User            `json:"user" gorm:"foreignKey:UserID"`
-	gorm.Model
+	ID            uint            `json:"id" gorm:"primaryKey"`
+	UserID        uint            `json:"user_id" gorm:"not null;index:idx_limit"`
+	Tenor         uint            `json:"tenor" gorm:"type:smallint unsigned;not null;index:idx_limit"`
+	CurrentLimit  decimal.Decimal `json:"current_limit" gorm:"type:decimal(20,2);not null"`
+	OriginalLimit decimal.Decimal `json:"original_limit" gorm:"type:decimal(20,2);not null"`
+
+	// Relationship
+	User User `json:"user" gorm:"foreignKey:UserID"`
+
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
 
 func (Limit) TableName() string {
 	return "limits"
-}
-
-// BeforeCreate is a GORM hook that is triggered before a new record is inserted into the database.
-// It generates a new UUID for the UUID field of the struct.
-func (l *Limit) BeforeCreate(tx *gorm.DB) (err error) {
-	if l.UUID == uuid.Nil {
-		l.UUID = uuid.New()
-	}
-	return
 }
--- interfaces/model/limit_model.go
+++ interfaces/model/limit_model.go
@@ -1,28 +1,27 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"github.com/sayyidinside/gofiber-clean-fresh/domain/entity"
 	"github.com/shopspring/decimal"
 )
 
 type (
 	LimitList struct {
-		ID          uint            `json:"id"`
-		UUID        uuid.UUID       `json:"uuid"`
-		UserID      uint            `json:"user_id"`
-		Tenor       uint            `json:"tenor"`
-		LimitAmount decimal.Decimal `json:"limit_amount"`
+		ID            uint            `json:"id"`
+		UserID        uint            `json:"user_id"`
+		Tenor         uint            `json:"tenor"`
+		CurrentLimit  decimal.Decimal `json:"current_limit"`
+		OriginalLimit decimal.Decimal `json:"original_limit"`
 	}
 )
 
 func LimitToListModel(limit *entity.Limit) *LimitList {
 	return &LimitList{
-		ID:          limit.ID,
-		UUID:        limit.UUID,
-		UserID:      limit.UserID,
-		Tenor:       limit.Tenor,
-		LimitAmount: limit.LimitAmount,
+		ID:            limit.ID,
+		UserID:        limit.UserID,
+		Tenor:         limit.Tenor,
+		CurrentLimit:  limit.CurrentLimit,
+		OriginalLimit: limit.OriginalLimit,
 	}
 }
 
