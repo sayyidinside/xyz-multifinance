ad4f8b0ead6f342c28204544311e7b0b1c423eac
sayyid <sayyid.visiku@gmail.com>
2025-07-09 14:35:10 +0700

    ➕ Create Transaction Entity

---
+++ domain/entity/transaction.go
@@ -0,0 +1,44 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
+)
+
+type TransactionStatus string
+
+const (
+	TransactionActive   TransactionStatus = "active"
+	TransactionPaid     TransactionStatus = "paid"
+	TransactionCanceled TransactionStatus = "canceled"
+)
+
+type Transaction struct {
+	ID               uint              `json:"id" gorm:"primaryKey"`
+	UUID             uuid.UUID         `json:"uuid" gorm:"uniqueIndex;type:char(36)"`
+	UserID           uint              `json:"user_id"`
+	AssetName        string            `json:"asset_name"`
+	ContractNumber   string            `json:"contract_number" gorm:"uniqueIndex"`
+	OnTheRoad        decimal.Decimal   `json:"on_the_road" gorm:"type:decimal(20,2)"`
+	AdminFee         decimal.Decimal   `json:"admin_fee" gorm:"type:decimal(20,2)"`
+	InstalmentAmount decimal.Decimal   `json:"instalment_amount" gorm:"type:decimal(20,2)"`
+	InterestAmount   decimal.Decimal   `json:"interest_amount" gorm:"type:decimal(20,2)"`
+	Tenor            uint              `json:"tenor" gorm:"type:smallint unsigned"`
+	Status           TransactionStatus `gorm:"type:transaction_status;default:'active'"`
+	User             User              `json:"user" gorm:"foreignKey:UserID"`
+	gorm.Model
+}
+
+func (Transaction) TableName() string {
+	return "transactions"
+}
+
+// BeforeCreate is a GORM hook that is triggered before a new record is inserted into the database.
+// It generates a new UUID for the UUID field of the struct.
+func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
+	if t.UUID == uuid.Nil {
+		t.UUID = uuid.New()
+	}
+	return
+}
--- domain/entity/user.go
+++ domain/entity/user.go
@@ -10,23 +10,24 @@ import (
 )
 
 type User struct {
-	ID          uint                `json:"id" gorm:"primaryKey"`
-	UUID        uuid.UUID           `json:"uuid" gorm:"uniqueIndex;type:char(36)"`
-	RoleID      uint                `json:"role_id"`
-	Username    string              `json:"username" gorm:"index"`
-	Name        string              `json:"name"`
-	LegalName   string              `json:"legal_name"`
-	Email       string              `json:"email" gorm:"index"`
-	Nik         string              `json:"nik" gorm:"index;type:varchar(16)"`
-	BirthPlace  string              `json:"birth_place"`
-	BirthDate   time.Time           `json:"birth_date" gorm:"type:date"`
-	Password    string              `json:"password"`
-	Salary      decimal.NullDecimal `json:"salary" gorm:"type:decimal(20,2)"`
-	ValidatedAt sql.NullTime        `json:"validated_at" gorm:"index"`
-	SelfieFile  string              `json:"selfie_file"`
-	KtpFile     string              `json:"ktp_file"`
-	Role        Role                `json:"role" gorm:"foreignKey:RoleID"`
-	Limits      []Limit             `json:"limits" gorm:"foreignKey:UserID"`
+	ID           uint                `json:"id" gorm:"primaryKey"`
+	UUID         uuid.UUID           `json:"uuid" gorm:"uniqueIndex;type:char(36)"`
+	RoleID       uint                `json:"role_id"`
+	Username     string              `json:"username" gorm:"index"`
+	Name         string              `json:"name"`
+	LegalName    string              `json:"legal_name"`
+	Email        string              `json:"email" gorm:"index"`
+	Nik          string              `json:"nik" gorm:"index;type:varchar(16)"`
+	BirthPlace   string              `json:"birth_place"`
+	BirthDate    time.Time           `json:"birth_date" gorm:"type:date"`
+	Password     string              `json:"password"`
+	Salary       decimal.NullDecimal `json:"salary" gorm:"type:decimal(20,2)"`
+	ValidatedAt  sql.NullTime        `json:"validated_at" gorm:"index"`
+	SelfieFile   string              `json:"selfie_file"`
+	KtpFile      string              `json:"ktp_file"`
+	Role         Role                `json:"role" gorm:"foreignKey:RoleID"`
+	Limits       []Limit             `json:"limits" gorm:"foreignKey:UserID"`
+	Transactions []Transaction       `json:"transactions" gorm:"foreignKey:UserID"`
 	gorm.Model
 }
 
