a5c1b10028b62e5c32500eec7463196946ef83e5
sayyid <sayyid.visiku@gmail.com>
2025-07-09 16:55:53 +0700

    ➕ Create User Document And Profile Migrations

--- infrastructure/database/migration.go
+++ infrastructure/database/migration.go
@@ -10,6 +10,8 @@ func Migrate(db *gorm.DB) {
 	db.AutoMigrate(&entity.Permission{})
 	db.AutoMigrate(&entity.Role{})
 	db.AutoMigrate(&entity.User{})
+	db.AutoMigrate(&entity.UserProfile{})
+	db.AutoMigrate(&entity.UserDocument{})
 	db.AutoMigrate(&entity.RefreshToken{})
 	db.AutoMigrate(&entity.Limit{})
 	db.AutoMigrate(&entity.Transaction{})
