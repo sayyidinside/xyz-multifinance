44be5be324ff3ff62a2ff73abeb10ceeea186d07
sayyid <sayyid.visiku@gmail.com>
2025-07-10 17:34:31 +0700

    ♻️ Refactor Registration Route To Implement Authentication And Authorization Middleware

--- interfaces/http/routes/v1/registrations/router.go
+++ interfaces/http/routes/v1/registrations/router.go
@@ -3,13 +3,31 @@ package registrations
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/sayyidinside/gofiber-clean-fresh/interfaces/http/handler"
+	"github.com/sayyidinside/gofiber-clean-fresh/interfaces/http/middleware"
 )
 
 func RegisterRoutes(route fiber.Router, handler handler.RegistrationHandler) {
 	registrationRoutes := route.Group("/registration")
 
 	registrationRoutes.Post("/", handler.Register)
-	registrationRoutes.Post("/activate/:uuid", handler.Activate)
-	registrationRoutes.Get("/", handler.GetAllRegistration)
-	registrationRoutes.Get("/:uuid", handler.GetRegistration)
+
+	registrationRoutes.Post(
+		"/activate/:uuid",
+		middleware.Authentication(),
+		middleware.Authorization(true, true, []string{}),
+		handler.Activate,
+	)
+	registrationRoutes.Get(
+		"/",
+		middleware.Authentication(),
+		middleware.Authorization(true, true, []string{}),
+		handler.GetAllRegistration,
+	)
+
+	registrationRoutes.Get(
+		"/:uuid",
+		middleware.Authentication(),
+		middleware.Authorization(true, true, []string{}),
+		handler.GetRegistration,
+	)
 }
